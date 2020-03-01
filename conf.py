5895683cc7b76facdc1143d36aff9e990719c271
Kaszanas <34846245+Kaszanas@users.noreply.github.com>
2020-03-01 16:58:01 +0100

    Nikola auto commit.
    Nikola version: 8.0.4

--- conf.py
+++ conf.py
@@ -17,7 +17,7 @@ import time
 
 # Data about this site
 BLOG_AUTHOR = "Andrzej Białecki"  # (translatable)
-BLOG_TITLE = "kaszanas"  # (translatable)
+BLOG_TITLE = "Kaszanas"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
 SITE_URL = "https://kaszanas.github.io/"
